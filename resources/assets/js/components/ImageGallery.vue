186bc4fc4dfcb9a657efcfc14bee598566957e87
Lasse <lasseloukonen@gmail.com>
2016-09-29 14:52:26 +0200

    god weekend

--- resources/assets/js/components/ImageGallery.vue
+++ resources/assets/js/components/ImageGallery.vue
@@ -10,13 +10,15 @@
     		<img 
 	        	v-bind:src="image.path"
 	    	>
-	    	    <div class="controls">
-	    	<a @click="prevImage">Prev</a>
-	    	<a @click="nextImage">Next</a>
-        </div>
-	    	<div class="col-md-6 description">
+	    	
+	    	<div class="description">
 			{{image.description}}
-			</div>
+		</div>
+	    <div class="controls">
+	    	
+    		<a @click="prevImage"><i class="icons fa fa-angle-left"></i></a>
+	    	<a @click="nextImage"><i class="icons fa fa-angle-right"></i></a>
+        </div>
 
     </div>
 </template>
@@ -40,16 +42,28 @@
 	    display: flex;
 	    justify-content: space-around;
 	}
-	.controls{
-		position: absolute;
-		bottom: -330px;
+	.controls{    
+		width: calc( 100% + 60px);
+	    position: absolute;
+	    display: flex;
+	    justify-content: space-between;
+	    height: 100%;
+	    align-items: center;
+	    margin-left: -30px;
 	}
 	.description {
-		/*flex-basis: 50%; */
 		color: #ffffff;
+		padding: 10px;
 	}
-	img {
 	
+	.icons {
+		color:  #ffffff;
+		font-size: 50px;
+		margin-right: 10px;
+	}
+	.icons:hover {
+		opacity: 0.5;
+		cursor: pointer;
 	}
 </style>
 <script>
@@ -62,19 +76,19 @@
 		        	{
 		        		title: 'Billede 1',
 		        		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In perferendis nihil ea id illum voluptatem totam at quae, harum a, voluptas, assumenda, eum facilis ipsum fugit odit dolorem magni mollitia?',
-		        		path: 'http://placehold.it/300x200',
+		        		path: 'http://placehold.it/500x500',
 		        		active: true
 		        	},
 		        	{
 		        		title: 'Billede 2',
 		        		description: 'Maecenas in vehicula lorem, sed laoreet metus. Aliquam sodales, est vel porttitor elementum, orci magna luctus orci, vel dignissim orci elit et lectus. Cras finibus felis id libero lacinia fringilla. Aliquam erat volutpat. In et tincidunt augue. Quisque gravida nec turpis ut volutpat. Nullam eu est auctor, facilisis urna at, sollicitudin felis. Sed tristique ligula in posuere fermentum.',
-		        		path: 'http://placehold.it/300x200',
+		        		path: 'http://placehold.it/500x500',
 		        		active: false
 		        	},
 		        	{
 		        		title: 'Billede 3',
 		        		description: 'Maecenas vitae purus ornare, euismod leo laoreet, auctor nulla. Vivamus viverra tortor a eros commodo, nec feugiat sapien aliquam. Phasellus consectetur nisi quis felis scelerisque, id ornare ex pretium. Mauris et ante eget ligula porttitor molestie. Integer malesuada nisi nunc, in pulvinar mi blandit ac.',
-		        		path: 'http://placehold.it/300x200',
+		        		path: 'http://placehold.it/500x500',
 		        		active: false
 		        	}
 	        	]
